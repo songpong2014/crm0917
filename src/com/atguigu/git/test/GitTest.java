7e95511486a4dd19fe1b806600692fb7fb562a9c
songpeng_2014 <370056000@qq.com>
2016-02-25 15:38:44 +0800

    update file 02
    update file 02

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("first start");
 		System.out.println("update file 01");
+		System.out.println("update file 02");
 	}
 
 }
